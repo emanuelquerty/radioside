80f23aff2f8cb6502998894c5c736f3a9f0160d4
emanuelquerty <emanuelinacio1@hotmail.com>
2020-08-04 11:54:16 -0700

    Updates

---
+++ precache-manifest.20f8259029eb94e7e1b2684713db6bb8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f56a898ace00101a1846774d8880cdf1",
+    "url": "/radioside/index.html"
+  },
+  {
+    "revision": "ea0e35dfa18fcbcfeb56",
+    "url": "/radioside/static/css/2.bbf03e9b.chunk.css"
+  },
+  {
+    "revision": "9702f0564de34edef86c",
+    "url": "/radioside/static/css/main.a17ce6b4.chunk.css"
+  },
+  {
+    "revision": "ea0e35dfa18fcbcfeb56",
+    "url": "/radioside/static/js/2.fcddab0d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/radioside/static/js/2.fcddab0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9702f0564de34edef86c",
+    "url": "/radioside/static/js/main.2cfb41ca.chunk.js"
+  },
+  {
+    "revision": "402eac9cc8fc7580fc05",
+    "url": "/radioside/static/js/runtime-main.929b2275.js"
+  },
+  {
+    "revision": "74871842a78648e265538f2752e22368",
+    "url": "/radioside/static/media/default-station-favicon.74871842.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/radio/precache-manifest.abf9424946d5bd64adea746a68bd0fdf.js"
+  "/radioside/precache-manifest.20f8259029eb94e7e1b2684713db6bb8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/radio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/radioside/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
